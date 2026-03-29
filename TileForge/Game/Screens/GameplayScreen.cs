5d0f172a164384efc490f58b211358e53db29692
Chris Anderson <papertarrasque@Tarrasque-Linux.dojo>
2026-03-29 08:37:39 -0700

    revert: remove out-of-scope GameplayScreen change from test update commit

--- TileForge/Game/Screens/GameplayScreen.cs
+++ TileForge/Game/Screens/GameplayScreen.cs
@@ -548,11 +548,7 @@ public class GameplayScreen : GameScreen
                     break;
 
                 case EntityType.Trigger:
-                    var triggerTarget = PropertyAccess.GetString(instance.Properties, PropertyKeys.TargetMap);
-                    if (!string.IsNullOrEmpty(triggerTarget))
-                        TryEntityMapTransition(play, instance);
-                    else
-                        LogAndFloat(play, $"Triggered {instance.DefinitionName}", Color.White, instance.X, instance.Y);
+                    TryEntityMapTransition(play, instance);
                     break;
 
                 case EntityType.Interactable:
